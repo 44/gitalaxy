8cb8fe8b1cf4597026afc9db19c4bfce7c404ef5
Andrey Ustinov <austinov@gmail.com>
2024-01-07 00:05:51 -0800

    chore: remove old method

--- generate_data.py
+++ generate_data.py
@@ -204,68 +204,6 @@ class Sky:
         self.save_changes(force=True)
         self.save_index()
 
-def get_history(root):
-    git_args = ['git', 'log', '--format==%ad %H %ae', '--date=format:%Y-%m-%d', '--name-status', '--reverse']
-    c = subprocess.run(git_args, cwd=sys.argv[1], capture_output=True, text=True)
-
-    changes = []
-    cnt = 0
-
-    lines = c.stdout.split('\n')
-    current = None
-    changed_files = []
-    removed_files = []
-    biggest = 0
-
-    for l in lines:
-        l = l.strip()
-        if len(l) == 0:
-            continue
-        print(l)
-        if l.startswith('='):
-            if len(changed_files) > 0 or len(removed_files) > 0:
-                if current[2] is not None:
-                    changes.append({"id": cnt, "date": current[0], "commit": current[1], "author": current[2], "touched": changed_files, "gone": removed_files})
-                size = len(changed_files) + len(removed_files)
-                if size > biggest:
-                    biggest = size
-                    print(f"New biggest: {biggest} by {current[2]} at {current[0]}")
-                removed_files = []
-                changed_files = []
-                cnt += 1
-
-            d = l[1:11]
-            cid = l[12:52]
-            author = l[53:].strip()
-            if "@" in author:
-                author = author.split("@")[0]
-                current = (d, cid, author)
-            else:
-                current = (d, cid, None)
-        else:
-            if l.startswith('D'):
-                removed_files.append(l[2:].strip())
-            else:
-                changed_files.append(l[2:].strip())
-    import json
-    ordered = sorted(changes, key=lambda x: x['id'], reverse=True)
-
-    file_cnt = 0
-    last_cutoff = 0
-    current_number = 0
-
-    for i in range(len(ordered)):
-        current_number += len(ordered[i]['touched']) + len(ordered[i]['gone'])
-        if current_number > 10000:
-            print(f"Saved {file_cnt}: {last_cutoff} - {i}")
-            json.dump(ordered[last_cutoff:i+1], open(f"changes_{file_cnt}.json", "w"), indent=4)
-            last_cutoff = i
-            current_number = 0
-            file_cnt += 1
-
-    print(f"Saved {file_cnt}: {last_cutoff} - {len(ordered)}")
-    json.dump(ordered[last_cutoff:], open(f"changes_{file_cnt}.json", "w"), indent=4)
-
 if __name__ == "__main__":
     sky = Sky(sys.argv[1])
     sky.load_galaxies()
